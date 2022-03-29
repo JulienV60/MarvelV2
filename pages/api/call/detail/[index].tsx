fce723a0d62ac590868b326c1227322cf158fdef
mansuy maxime <mmansuy@norauto.fr>
2022-03-29 23:13:52 +0200

    maj

--- pages/api/call/detail/[index].tsx
+++ pages/api/call/detail/[index].tsx
@@ -12,12 +12,12 @@ export default async function handler(
         `${process.env.REACT_APP_MARVEL_PRIVATE_KEY}` +
         `${process.env.REACT_APP_MARVEL_PUBLIC_KEY}`
     );
-
+    console.log(`https://gateway.marvel.com/v1/public/${req.query.rubrique}/${req.query.index}?ts=${time}&apikey=${process.env.REACT_APP_MARVEL_PUBLIC_KEY}&hash=${hash}`)
     const response = await fetch(
       `https://gateway.marvel.com/v1/public/${req.query.rubrique}/${req.query.index}?ts=${time}&apikey=${process.env.REACT_APP_MARVEL_PUBLIC_KEY}&hash=${hash}`
     )
       .then((response) => response.json())
-      .then((result) => result.data.results);
+      .then((result) => result.data?.results);
 
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify({ data: response }));
